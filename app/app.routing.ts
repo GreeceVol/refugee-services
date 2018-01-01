922031af4646b0c3a4ab44c0c285bc6c1abea598
Vinay Gopinath <vinayg18@gmail.com>
2018-01-01 22:50:59 +0530

    [Side menu] Removed unused routes & labels

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,8 +1,8 @@
-import { HistoryComponent } from './pages/history/history.component';
+// import { SettingsComponent } from './pages/settings/settings.component';
+// import { HistoryComponent } from './pages/history/history.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ServiceComponent } from './pages/service/service.component';
-import { SettingsComponent } from './pages/settings/settings.component';
 import { Key } from './shared/constants/key.constants';
 
 export const routes = [
@@ -10,14 +10,14 @@ export const routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: `service/:${Key.SERVICE_ID_PARAM}`, component: ServiceComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'history', component: HistoryComponent }
+  // { path: 'settings', component: SettingsComponent },
+  // { path: 'history', component: HistoryComponent }
 ];
 
 export const navigableComponents = [
-  HistoryComponent,
   HomeComponent,
   LoginComponent,
-  ServiceComponent,
-  SettingsComponent
+  ServiceComponent
+  // SettingsComponent
+  // HistoryComponent,
 ];
